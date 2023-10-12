24b66640c1aac58b69807416b100514972a1e144
Vicente Plata <vp@codeforward.io>
2023-10-11 22:30:23 -0600

    improved punks table

--- app/explore/[wallet]/page.tsx
+++ app/explore/[wallet]/page.tsx
@@ -1,5 +1,6 @@
 import client from "../../../lib/client";
 import DisconnectButton from "./disconnect-button";
+import PunkRow from "./punk-row";
 
 interface ExplorerProps {
   params: {
@@ -29,10 +30,35 @@ const getPunks = async (wallet: string) => {
  */
 const Explorer = async ({ params: { wallet } }: ExplorerProps) => {
   const punks = await getPunks(wallet);
+  if (punks.length === 0) {
+    return (
+      <div className="min-h-screen bg-gray-100 p-4">
+        <h2 className="text-2xl font-semibold mb-4">Exploring {wallet}</h2>
+        <div className="bg-white rounded-lg shadow p-4">
+          <h3 className="text-lg font-semibold mb-4">No punks 😔</h3>
+        </div>
+      </div>
+    );
+  }
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
-      <p>Exploring {wallet}</p>
-      <p>{punks.length} punks</p>
+    <div className="min-h-screen bg-gray-100 p-4">
+      <h2 className="text-2xl font-semibold mb-4">Exploring {wallet}</h2>
+      <div className="bg-white rounded-lg shadow p-4">
+        <h3 className="text-lg font-semibold mb-4">Owned Punks</h3>
+        <table className="w-full">
+          <thead>
+            <tr>
+              <th className="text-left">Token ID</th>
+              <th className="text-left">Click to Explore</th>
+            </tr>
+          </thead>
+          <tbody>
+            {punks.map(({ id, tokenId }) => (
+              <PunkRow tokenId={tokenId} key={id} />
+            ))}
+          </tbody>
+        </table>
+      </div>
       <DisconnectButton />
     </div>
   );
---
+++ app/explore/[wallet]/punk-row.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { motion } from "framer-motion";
+
+interface PunkRowProps {
+  tokenId: string;
+}
+
+/**
+ * A row in the punks table
+ */
+const PunkRow = ({ tokenId }: PunkRowProps) => {
+  return (
+    <motion.tr
+      key={tokenId}
+      initial={{ opacity: 0, y: -20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.5, delay: 0.1 }}
+    >
+      <td>{tokenId}</td>
+      <td>
+        <a
+          href={`/explore/${tokenId}`}
+          className="text-blue-500 hover:underline"
+        >
+          Explore
+        </a>
+      </td>
+    </motion.tr>
+  );
+};
+
+export default PunkRow;
